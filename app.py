6970a10b5ad6668e7e8ec72a93d954335efb5b70
Friend Computer <21309768+IntoCpp@users.noreply.github.com>
2024-02-20 08:56:13 -0500

    All information done

--- app.py
+++ app.py
@@ -34,7 +34,41 @@ def hello():
       service = pcg.service_group, 
       service_desc = pcg.service_group_description,
       btn_treason = pcg.button_treason, 
-      btn_violence = pcg.button_violence)
+      btn_violence = pcg.button_violence, 
+
+      Brains = pcg.productivity.brains["Brains"],
+      Chutzpah = pcg.productivity.chutzpah["Chutzpah"],
+      Mechanics = pcg.productivity.mechanics["Mechanics"],
+      Violence = pcg.productivity.violence["Violence"], 
+
+      Alpha = pcg.productivity.brains["Alpha Complex"],
+      Bluff = pcg.productivity.chutzpah["Bluff"],
+      Demolition = pcg.productivity.mechanics["Demolitions"],
+      Athletics = pcg.productivity.violence["Athletics"], 
+
+      Bureaucracy = pcg.productivity.brains["Bureaucracy"],
+      Charm = pcg.productivity.chutzpah["Charm"],
+      Engineer = pcg.productivity.mechanics["Engineer"],
+      Guns = pcg.productivity.violence["Guns"],
+
+      Psychology = pcg.productivity.brains["Psychology"],
+      Intimidate = pcg.productivity.chutzpah["Intimidate"],
+      Operate = pcg.productivity.mechanics["Operate"],
+      Melee = pcg.productivity.violence["Melee"],
+
+      Science = pcg.productivity.brains["Science"],
+      Stealth = pcg.productivity.chutzpah["Stealth"],
+      Program = pcg.productivity.mechanics["Program"],
+      Throw = pcg.productivity.violence["Throw"], 
+
+      Secret = pcg.secret_society, 
+      Secret_desc = pcg.secret_society_description,
+      Mutant = pcg.mutations,
+      Mutant_purpose = pcg.mutations_purpose,
+      Mutant_warning = pcg.mutations_warning
+
+      )
+      
 
 
 if __name__ == '__main__':
